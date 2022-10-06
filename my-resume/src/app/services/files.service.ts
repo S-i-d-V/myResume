72607c7af25095da3d25d4f883a130ef68ab7e61
ugtheven <ugotheveny@student.42.fr>
2022-10-06 02:02:21 +0200

    Fixed bug with selection

--- my-resume/src/app/services/files.service.ts
+++ my-resume/src/app/services/files.service.ts
@@ -9,8 +9,8 @@ export class FilesService {
   files: File[] = [
     {
       fileName: 'me.ts',
-      opened: true,
-      selected: true
+      opened: false,
+      selected: false
     }, {
       fileName: 'education.ts',
       opened: false,
@@ -34,37 +34,38 @@ export class FilesService {
     //
   }
 
-  selectEditor(fileName: string): void{
+  selectFile(fileName: string): void{
     this.files.forEach((file) => file.selected = false);
     let foundIndex = this.files.findIndex((file) => file.fileName === fileName);
     if (foundIndex != -1){
       this.files[foundIndex].selected = true;
-      this.openEditor(fileName);
+      this.openTab(fileName);
     }
   }
 
-  closeEditor(fileName: string): void{
+  closeTab(fileName: string): void{
     let foundIndex = this.files.findIndex((file) => file.fileName === fileName);
     if (foundIndex != -1){
       this.files[foundIndex].opened = false;
       if (this.files[foundIndex].selected){
         this.files[foundIndex].selected = false;
         for (let file of this.files){
-          if (file.opened)
+          if (file.opened){
             file.selected = true;
-          break;
+            break;
+          }
         }
       }
     }
   }
 
-  openEditor(fileName: string): void{
+  openTab(fileName: string): void{
     let foundIndex = this.files.findIndex((file) => file.fileName === fileName);
     if (foundIndex != -1)
       this.files[foundIndex].opened = true;
   }
 
-  getSelectedFile(): string{
+  getSelectedTab(): string{
     let foundIndex = this.files.findIndex((file) => file.selected === true);
     if (foundIndex != -1)
       return (this.files[foundIndex].fileName);
