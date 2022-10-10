19e74eee1db365e35bd70ba71c9f15b0308e1aa7
ugtheven <ugotheveny@student.42.fr>
2022-10-10 02:18:43 +0200

    Implementation of tabs

--- my-resume/src/app/app.module.ts
+++ my-resume/src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { SelectorComponent } from './components/selector/selector.component';
 import { TabsService } from './services/tabs.service';
+import { TabComponent } from './components/tab/tab.component';
+import { MeComponent } from './components/tab/me/me.component';
+import { ContactComponent } from './components/tab/contact/contact.component';
+import { SkillsComponent } from './components/tab/skills/skills.component';
+import { EducationComponent } from './components/tab/education/education.component';
+import { ExperienceComponent } from './components/tab/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     SelectorComponent,
+    TabComponent,
+    MeComponent,
+    ContactComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperienceComponent,
   ],
   imports: [
     BrowserModule,
