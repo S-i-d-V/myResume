2cd83b7453eccf24b8d3d880b38fc31da3c5d141
ugtheven <ugotheveny@student.42.fr>
2022-10-10 02:35:38 +0200

    Fix .gitignore

---
+++ my-resume/src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { TabsService } from 'src/app/services/tabs.service';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  editorOpen: boolean = true;
+  filesOpen: boolean = true;
+
+  constructor(public tabsService: TabsService) { }
+
+  ngOnInit(): void {
+  }
+
+  switchEditor(): void {
+    this.editorOpen = !this.editorOpen;
+  }
+
+  switchFiles(): void {
+    this.filesOpen = !this.filesOpen;
+  }
+
+}
