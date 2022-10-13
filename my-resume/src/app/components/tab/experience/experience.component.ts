e9556b9740904011443ba99e459f35956943fc23
ugtheven <ugotheveny@student.42.fr>
2022-10-13 02:47:44 +0200

    Translated text to french

--- my-resume/src/app/components/tab/experience/experience.component.ts
+++ my-resume/src/app/components/tab/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TabsService } from 'src/app/services/tabs.service';
 
 @Component({
   selector: 'app-experience',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  constructor(public tabsService: TabsService) { }
 
   ngOnInit(): void {
   }
--- my-resume/src/app/components/tab/me/presentation.component.ts
+++ my-resume/src/app/components/tab/me/presentation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TabsService } from 'src/app/services/tabs.service';
 
 @Component({
   selector: 'app-me',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public tabsService: TabsService) { }
 
   ngOnInit(): void {
   }
